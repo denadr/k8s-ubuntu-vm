4427c23f8d9c6b47658db78b72b50c45f7443486
Dennis Adrians <dennis.adrians@quibiq.de>
2020-07-28 11:28:32 +0200

    fix typo

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo apt install unzip
-curl -LOk https://github.com/denadr/k8s-ubuntu-vm/archive/master.zipy
+curl -LOk https://github.com/denadr/k8s-ubuntu-vm/archive/master.zip
 unzip master.zip -d .
 mv k8s-ubuntu-vm-master/ubuntu-k8s-setup.sh ubuntu-k8s-setup.sh
 chmod u+r+x ubuntu-k8s-setup.sh
